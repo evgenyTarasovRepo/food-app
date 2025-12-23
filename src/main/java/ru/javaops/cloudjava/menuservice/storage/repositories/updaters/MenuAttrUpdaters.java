8f8a2b2e9afe370f3de4d6773bcfdb6e6db964f5
Evgeny Tarasov <e.tarasov@x5.ru>
2025-12-23 16:09:34 +0300

    data_layer_HW_2 add update and sort methods

---
+++ src/main/java/ru/javaops/cloudjava/menuservice/storage/repositories/updaters/MenuAttrUpdaters.java
@@ -0,0 +1,38 @@
+package ru.javaops.cloudjava.menuservice.storage.repositories.updaters;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.javaops.cloudjava.menuservice.dto.UpdateMenuRequest;
+import ru.javaops.cloudjava.menuservice.model.MenuItem_;
+
+import java.math.BigDecimal;
+
+@Configuration
+    public class MenuAttrUpdaters {
+        @Bean
+        MenuAttrUpdater<String> description() {
+            return new MenuAttrUpdater<>(UpdateMenuRequest::getDescription, MenuItem_.description);
+        }
+
+        @Bean
+        MenuAttrUpdater<String> imageUrl() {
+            return new MenuAttrUpdater<>(UpdateMenuRequest::getImageUrl, MenuItem_.imageUrl);
+        }
+
+        @Bean
+        MenuAttrUpdater<String> name() {
+            return new MenuAttrUpdater<>(UpdateMenuRequest::getName, MenuItem_.name);
+        }
+
+        @Bean
+        MenuAttrUpdater<BigDecimal> price() {
+
+            return new MenuAttrUpdater<>(UpdateMenuRequest::getPrice, MenuItem_.price);
+        }
+
+        @Bean
+        MenuAttrUpdater<Long> timeToCook() {
+
+            return new MenuAttrUpdater<>(UpdateMenuRequest::getTimeToCook, MenuItem_.timeToCook);
+        }
+    }
